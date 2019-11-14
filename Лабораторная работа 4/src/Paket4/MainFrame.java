be5a92104590a2b408818bd52dab5521da8b0307
Cronus023 <anlrey.klochko@mail.ru>
2019-11-14 23:07:29 +0300

    add the fields of main class

--- Лабораторная работа 4/src/Paket4/MainFrame.java
+++ Лабораторная работа 4/src/Paket4/MainFrame.java
@@ -1,9 +1,24 @@
 package Paket4;
 
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame {
-
+	// the size of window
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 600;
+	// the object of dialog box to select files
+	private JFileChooser fileChooser = null;
+	// menu items
+	private JCheckBoxMenuItem showAxisMenuItem;
+	private JCheckBoxMenuItem showMarkersMenuItem;
+	// component - graphics display
+	private GraphicsDisplay display = new GraphicsDisplay();
+	// flag, which show file loaded or no
+	private boolean fileLoaded = false;
+	
+	
 	public static void main(String[] args) {
 		
 	}
